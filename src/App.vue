282f28b79dfd0baa3de6acb8c5e82046af909719
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-08-21 13:40:50 +0700

    feat: basic vue graphql

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Example</h3>
+    <div>
+      Data Language: {{ example1 }}
+      <br>
+      Data Champions : {{ JSON.stringify(champions) }}
+      <br>
+      Data Champion : {{ JSON.stringify(champion) }}
+    </div>
+    <button @click="getLanguage">Get Language</button>
+    <button @click="getChampions">Get Champions</button>
+    <button @click="getChampionByName">Get Champion</button>
+    <div>
+      <h3>Example Mutation</h3>
+      Name: <input v-model="name">
+      Attack Damage: <input v-model.number="attack">
+      <div>
+        Data:
+        {{ updatedChampion }}
+      </div>
+      <button @click="updateAttackDamage">Update Champion</button>
+    </div>
+    <hr> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      example1: '',
+      champions: null,
+      champion: '',
+      name: '',
+      attack: null,
+      updatedChampion: null
+    }
+  },
+  methods: {
+    async updateAttackDamage () {
+      const res = await axios.post('http://localhost:4000/graphql', {
+        query: `
+        mutation UpdateAttackDamage($championName: String!, $attackDamage: Float) {
+          updateAttackDamage(name: $championName, attackDamage: $attackDamage) {
+            name
+            attackDamage
+          }
+        }`,
+        variables: {
+          championName: this.name,
+          attackDamage: this.attack
+        }
+      })
+      this.updatedChampion = res.data.data.updateAttackDamage
+    },
+    async getLanguage () {
+      try {
+        const res = await axios.post(
+          'http://localhost:4000/graphql', {
+          query: '{ language }'
+        })
+        // console.log(res.data.data)
+        this.example1 = res.data.data.language
+      } catch (e) {
+        console.log('err', e)
+      }
+    },
+    async getChampions () {
+      const res = await axios.post(
+        'http://localhost:4000/graphql', {
+        query: `{
+          getChampions {
+            name
+          }
+        }`
+      })
+      this.champions = res.data.data.getChampions
+    },
+    async getChampionByName () {
+      const res = await axios.post('http://localhost:4000/graphql', {
+        query: `
+          query GetChampionByName($championName: String!) {
+            getChampionByName(name: $championName) {
+              name
+              attackDamage
+            }
+          }`,
+          variables: {
+            championName: 'Ashe'
+          }
+      })
+      this.champion = res.data.data.getChampionByName
+    }
   }
 }
 </script>
